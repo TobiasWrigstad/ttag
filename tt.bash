ff352237c1c31e5517af2fff50ffe1fe88eb7db9
Tobias Wrigstad <tobias.wrigstad@it.uu.se>
2024-12-23 14:23:27 +0100

    Fixed echo discrepancy in bash;zsh + added column match

--- tt.bash
+++ tt.bash
@@ -19,20 +19,21 @@ HIGHLIGHT_WORD="${@: -1}"
 > "$ALIAS_FILE"
 
 # Run ag and process each line
-/usr/bin/ag --group $* | while IFS= read -r line; do
+/usr/bin/ag --column --group $* | while IFS= read -r line; do
     # Print the original line (preserve any colors X produces)
     
     # If the line contains ":", it must be linenumber:match
-    if [[ "$line" =~ ^([0-9]+):(.+)$ ]]; then
+    if [[ "$line" =~ ^([0-9]+):([0-9]+):(.+)$ ]]; then
         # Extract linenumber and match from the line
         linenumber="${BASH_REMATCH[1]}"
-        match="${BASH_REMATCH[2]}"
+        colnumber="${BASH_REMATCH[2]}"
+        match="${BASH_REMATCH[3]}"
         
         # Add the alias definition to the external file
-        echo "alias e$i='$EDITOR $filename +$linenumber'" >> "$ALIAS_FILE"
+        echo "alias e$i='$EDITOR $filename:$linenumber:$colnumber'" >> "$ALIAS_FILE"
 
         # Escape only the backslash character in the match
-        escaped_match=$(echo "$match" | sed 's/\\/\\\\/g')
+        escaped_match=$(printf "%s" "$match" | sed 's/\\/\\\\/g')
 
         # Increment the counter for the next alias
         echo -e "${BLUE}[${MAGENTA}$i${BLUE}]${RESET} ${YELLOW}$linenumber${RESET}:$escaped_match" | GREP_COLOR='13;30;43' grep --color=always -E "\b$HIGHLIGHT_WORD\b"
